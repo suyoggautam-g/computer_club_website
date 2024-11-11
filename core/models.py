f9dd9b0638b8658722e579efd5d315cd586f6d02
suyoggautam-g <suyoggautam08201@gmail.com>
2024-11-11 01:47:38 -0500

    Added RSVP functionality and My RSVPs page~

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User  # Import the User model
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    location = models.CharField(max_length=200)
+    participants = models.ManyToManyField(User, related_name="events_participated", blank=True)
+
+    def __str__(self):
+        return self.title
--- core/views.py
+++ core/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Event
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
+@login_required
+def my_rsvps(request):
+    # Get all events that the logged-in user has RSVP'd to
+    events = request.user.events_participated.all()
+    return render(request, 'core/my_rsvps.html', {'events': events})
+
+@login_required
+def rsvp_event(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    if request.user in event.participants.all():
+        event.participants.remove(request.user)
+    else:
+        event.participants.add(request.user)
+    # Redirect to "My RSVPs" page after RSVP action
+    return redirect('my_rsvps')
+
 
 def home(request):
     return render(request, 'core/home.html')
@@ -12,3 +38,37 @@ def events(request):
 def join(request):
     return render(request, 'core/join.html')
 
+@login_required
+def events_list(request):
+    events = Event.objects.all().order_by('date')  # Order events by date
+    return render(request, 'core/events_list.html', {'events': events})
+    
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in the user after registration
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'core/register.html', {'form': form})
+    
+@login_required
+def rsvp_event(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    if request.user in event.participants.all():
+        event.participants.remove(request.user)
+    else:
+        event.participants.add(request.user)
+    return HttpResponseRedirect(reverse('events'))  # Redirect back to the events list
+    
+@login_required
+def rsvp_event(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    if request.user in event.participants.all():
+        event.participants.remove(request.user)
+    else:
+        event.participants.add(request.user)
+    # Redirect to "My RSVPs" page after RSVP action
+    return redirect('my_rsvps')
